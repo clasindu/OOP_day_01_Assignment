b0a2883bd711b2b61eaa32b546f061055f324628
clasindu <lasindura00@gmail.com>
2026-06-20 14:28:16 +0530

    complete CardPayment class

---
+++ src/Payment/CardPayment.java
@@ -0,0 +1,50 @@
+package Payment;
+
+public class CardPayment extends Payment{
+
+    public String cardNumber;
+    public String cardHolderName;
+    public String expiryDate;
+
+    public CardPayment(double amount , String currency , String status, String cardNumber ,String cardHolderName, String expiryDate){
+
+        super(amount, currency, status);
+        this.cardNumber     = cardNumber;
+        this.cardHolderName =cardHolderName;
+        this.expiryDate     = expiryDate;
+    }
+
+    public String getCardNumber(){
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber){
+        this.cardNumber = cardNumber;
+    }
+
+    public String getCardHolderName(){
+        return cardHolderName;
+    }
+
+    public void setCardHolderName(String cardHolderName){
+        this.cardHolderName = cardHolderName;
+    }
+
+    public String getExpiryDate(){
+        return expiryDate;
+    }
+
+    public void setExpiryDate(String expiryDate){
+        this.expiryDate =expiryDate;
+    }
+
+    @Override
+    public void processPayment(){
+        System.out.println("Authorizing card payment for" + cardHolderName);
+
+    }
+
+    public void validateCard(){
+        System.out.println("Validating card number format...");
+    }
+}
